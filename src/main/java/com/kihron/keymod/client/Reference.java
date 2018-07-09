29b5f4966b86ef72d18e59f68c8abb0baf97594d
Kihron <starlicblackhole@gmail.com>
2018-07-09 18:49:14 -0400

    Updated Version Strings
    Changed version strings to "1.6.4"

--- src/main/java/com/kihron/keymod/client/Reference.java
+++ src/main/java/com/kihron/keymod/client/Reference.java
@@ -3,10 +3,10 @@ package com.kihron.keymod.client;
 public class Reference {
     public static final String MOD_ID = "kbm";
     public static final String MOD_NAME = "KeyBinds Mod";
-    public static final String MOD_VERSION = "1.6.3-1.8.9";
+    public static final String MOD_VERSION = "1.6.4-1.8.9";
     public static final String CLIENT_PROXY_CLASS = "com.kihron.keymod.proxy.ClientProxy";
     public static final String SERVER_PROXY_CLASS = "com.kihron.keymod.proxy.CommonProxy";
-    public static final String VERSION = "1.6.3";
+    public static final String VERSION = "1.6.4";
     public static final String GUI_FACTORY = "com.kihron.keymod.client.config.GuiFactory";
     public static final String ACCEPTED_MC_VERSIONS = "[1.8.9]";
 }
